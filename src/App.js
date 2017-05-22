c18aa6643610245155cb47519311f43d446a9a83
Manveer695 <singhmanveer1995@gmail.com>
2017-05-22 11:57:50 +0530

    including axios

--- src/App.js
+++ src/App.js
@@ -2,20 +2,103 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+var axios = require('axios');
+
+const Card = (props) => {
+    let ans,speaker;
+        if(props.data.answers){
+                speaker = "Susi";
+                ans = props.data.answers[0].actions[0].expression;
+            }
+            else{
+                speaker = "You";
+                ans = props.data;
+            }
+    return(
+  	<div style = {{margin: '1em'}}>
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}> 
+          {
+            speaker    
+          }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div> 
+          { 
+            ans
+          } 
+        </div>
       </div>
+    </div>
+  );
+};
+
+const CardList = (props) => {
+	return(
+        <div>
+          {props.cards.map(card => <Card {...card}/>)}
+        </div>
     );
-  }
+}
+
+class Form extends React.Component{
+	constructor(){
+        super();
+        this.state = {
+                        userName: ''
+        }
+        this.handleSubmit = (event) => {
+            event.preventDefault();
+            let obj = {};
+            obj.data = this.state.userName;
+            this.props.cards(obj);
+            axios.get(`http://api.asksusi.com/susi/chat.json?timezoneOffset=-330&q=${this.state.userName}`)
+            .then( resp => {
+                this.props.cards(resp);
+                this.setState({
+                    userName: ''
+                });
+            });
+        }
+    }
+	render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <input 
+            value = {this.state.userName}
+            onChange = {(event) => {
+                                      this.setState({
+                                        userName: event.target.value
+                                      })
+                                                         }
+                                 }
+            type = "text" placeholder="Github Username" required/>
+            <button type="submit">Add question</button>
+          </form>
+        );
+    }
+}
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+         cards: []
+        };
+
+        this.addNewCard = (cardInfo) => {
+            this.setState(prevState => ({
+                cards: prevState.cards.concat(cardInfo)
+            }));
+        };
+    }
+    render(){
+        return(
+            <div>
+            <Form cards={this.addNewCard}/>
+            <CardList cards={this.state.cards}/>
+          </div>
+        );
+    }
 }
 
 export default App;
